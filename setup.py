2a5ee7260529a25b6a09f180a076d0a4e4975965
Henrik Skupin <mail@hskupin.info>
2012-10-01 16:32:37 +0200

    Add addons testrun

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(name=NAME,
       entry_points="""
       # -*- Entry points: -*-
       [console_scripts]
+      testrun_addons = mozmill_automation:addons_cli
       testrun_endurance = mozmill_automation:endurance_cli
       testrun_functional = mozmill_automation:functional_cli
       testrun_l10n = mozmill_automation:l10n_cli
